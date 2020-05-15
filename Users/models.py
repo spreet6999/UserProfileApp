923308e56a6679f07b0112d6c4c42f5aedf605f8
spreet6999 <42646816+spreet6999@users.noreply.github.com>
2020-05-15 11:39:26 +0530

    First Commit

---
+++ Users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib import auth
+from django.utils import timezone
+
+# Create your models here.
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    information = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
---
+++ Users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+# Create your views here.
+from django.contrib.auth import login, logout
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView
+from . import forms
+from .models import Profile, User
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "Users/base.html", {})
+
+class ProfileDetail(DetailView):
+    model = User
+    template_name='Users/profile.html'
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "Users/signup.html"
